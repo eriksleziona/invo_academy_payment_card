641a049277767f410010e7ef7c56d4aaad48991f
Erik Sleziona <erik.sleziona@gmail.com>
2023-03-21 10:15:52 +0100

    Adding styling and started to write basic JS Validation

---
+++ src/pages/home-page/scripts/form-validation.js
@@ -0,0 +1,30 @@
+
+export default class Validators {
+
+    validateCardNumber (creditCardNumber) {
+        // - Remove any '-' and ' ' fron the card Number String
+        creditCardNumber = creditCardNumber.replace(/\D/g, '')
+        // Checking for the length beetween 13-19 Digits
+        if(!/^\d{13,19}$/g.test(creditCardNumber)) {
+            return false
+        }   
+        // Split and reverse the Credit Card NUmber
+        let reversedCardNumberArr = creditCardNumber.split('').reverse().map(Number)
+
+        let sum = reversedCardNumberArr.reduce((acc, currentVal, currentIndex) => {
+            
+            if(currentIndex % 2 === 1) {            
+                let doubleVal = currentVal * 2
+                return acc + (doubleVal > 9 ? doubleVal - 9 : doubleVal)
+            }else {
+                return acc + currentVal
+            }
+        })
+        return sum % 10 === 0
+    }
+
+    validateCVV (cvv) {
+        cvv = cvv.replace(/\D/g, '')
+       return /^\d{3,4}$/g.test(cvv) ? true : false
+    }
+}
--- src/pages/home-page/scripts/home-page.js
+++ src/pages/home-page/scripts/home-page.js
@@ -3,6 +3,23 @@
 
 import '../styles/home-page.scss'
 
-import { setupCounter } from '../../../global-scripts/scripts/counter/counter.js'
+import  Validators from '../scripts/form-validation'
 
-setupCounter(document.querySelector('#counter'))
+
+const triggers = {
+    'card' : document.getElementById('card'),
+    'cvv': document.getElementById('cvv')
+}
+
+const val = new Validators()
+
+triggers.card.addEventListener('keyup', () => {
+    let num = triggers.card.value;
+    console.log(val.validateCardNumber(num))
+    
+})
+
+triggers.cvv.addEventListener('keyup', () => {
+    let cvvNum = triggers.cvv.value;
+    console.log(val.validateCVV(cvvNum))
+})
